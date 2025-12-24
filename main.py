5c67c570d41a5eda14ef03a08bcaeaa5bc17bfa2
Ella Ksenofontova <ellaksenofontova@gmail.com>
2025-12-24 20:34:12 +0300

    Updated algorithm and added messages

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import dist
+SCALE = 25
+
 def get_dimensions_from_file(path: str) -> tuple[float]:
     with open(path) as rect_coords_file:
         width, height = map(int, rect_coords_file.readline().split())
@@ -25,8 +28,8 @@ def make_matrix(w: int, h: int) -> list[list[int]]:
     return matrix
 
 
-def fill_max_free_space(matrix: list[list[int]], radius: float):
-    square_side = round(radius * 2)
+def get_start_point(matrix: list[list[int]], radius: float) -> list[int]:
+    square_side = round(radius * SCALE * 2)
     parts = []
 
     for i in range(len(matrix)):
@@ -46,14 +49,33 @@ def fill_max_free_space(matrix: list[list[int]], radius: float):
             parts.append(params)
 
     parts.sort(reverse=1, key=lambda p: p["number_of_zeros"])
+
     needed_start_point = parts[0]["start_point"]
-    start_y, start_x = needed_start_point
+    return needed_start_point
+
+
+def get_center(start_point: list[int], radius: float) -> list[int]:
+    start_y, start_x = start_point
+    center = [start_x + radius * SCALE, start_y + radius * SCALE]
+    return center
+
+
+def fill_max_free_space(matrix: list[list[int]], radius: float):
+    square_side = round(radius * SCALE * 2)
+    
+    start_point = get_start_point(matrix, radius)
+    start_y, start_x = start_point
+    center = get_center(start_point, radius)
+    print(f"Центр базовой станции: {center[0] / SCALE}, {center[1] / SCALE}")
+
     range_y = min(start_y + square_side, len(matrix))
-    range_x = min(start_x + square_side, len(matrix[i]))
+    range_x = min(start_x + square_side, len(matrix[0]))
 
     for i in range(start_y, range_y):
         for j in range(start_x, range_x):
-            matrix[i][j] = 1
+            distance = dist(center, [j, i])
+            if distance <= radius * SCALE:
+                matrix[i][j] = 1
 
 
 def get_count_of_zeros(matrix: list[list[int]]) -> int:
@@ -65,21 +87,13 @@ def main():
     stations = get_stations_from_file("./tests/1/test_radius.txt")
     used_stations = 0
 
-    matrix = make_matrix(width, height)
+    matrix = make_matrix(width * SCALE, height * SCALE)
 
     while len(stations) > 0 and get_count_of_zeros(matrix):
-        print(f"Шаг {used_stations + 1}")
-        print(f"Радиус станции: {stations[0]}")
+        print(f"Шаг №{used_stations + 1}")
         fill_max_free_space(matrix, stations[0])
         stations.pop(0)
         used_stations += 1
-
-        for m in matrix:
-            print(m)
-
-        print()
-        print("*" * (len(matrix[0]) * 3) )
-        print()
 
     print("Использовано станций: ", used_stations)
 
